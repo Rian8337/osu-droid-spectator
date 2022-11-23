8008d8dd3e6514a2b7b66f428131b91f679ba687
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-11-23 21:23:07 +0800

    Fix progress bar cap not setting audio state

--- src/ts/elements/ProgressBar.ts
+++ src/ts/elements/ProgressBar.ts
@@ -2,7 +2,7 @@ import { dataProcessor } from "../settings/SpectatorSettings";
 import { audioState } from "./Audio";
 
 (<JQuery<HTMLInputElement>>$("#progress")).on("change", function () {
-    const value = parseInt(this.value);
+    let value = parseInt(this.value);
 
     if (dataProcessor) {
         // Don't forward too far if spectator data is not available yet.
@@ -10,11 +10,11 @@ import { audioState } from "./Audio";
         const { latestEventTime } = dataProcessor;
 
         if (latestEventTime !== null && value > latestEventTime) {
-            this.value = (latestEventTime / 1000).toString();
-            return;
+            value = latestEventTime / 1000;
         }
     }
 
+    this.value = value.toString();
     audioState.audio.currentTime = value;
     $(audioState.audio).trigger("play");
 });
