93424c48f5d185f38fb08253ed6bb25694951c1f
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2022-12-25 14:09:02 +0800

    Keep play controls for 2 seconds

--- src/ts/elements/Body.ts
+++ src/ts/elements/Body.ts
@@ -1,9 +1,18 @@
+import { audioState } from "./Audio";
+
 $(document.body).on("mousemove", function (e) {
     const self = $(this);
 
-    if (e.clientY >= 0.9 * innerHeight || e.clientY <= 0.1 * innerHeight) {
-        self.addClass("h");
-    } else {
-        self.removeClass("h");
+    if ((e.clientY >= 0.95 * innerHeight || e.clientY <= 0.05 * innerHeight)) {
+        clearTimeout(self.data("h"));
+
+        self.addClass("h").data(
+            "h",
+            setTimeout(() => {
+                if (!audioState.audio.paused) {
+                    self.removeClass("h");
+                }
+            }, 2000)
+        );
     }
 });
