83150258e9b319ca220827c546bb831b5ea4dcf7
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-04-15 23:28:47 +0800

    Simplify logic

--- src/ts/elements/Body.ts
+++ src/ts/elements/Body.ts
@@ -1,18 +1,20 @@
 import { audioState } from "./Audio";
 
 $(document.body).on("mousemove", function (e) {
+    if (e.clientY < 0.95 * innerHeight && e.clientY > 0.05 * innerHeight) {
+        return;
+    }
+
     const self = $(this);
 
-    if (e.clientY >= 0.95 * innerHeight || e.clientY <= 0.05 * innerHeight) {
-        clearTimeout(self.data("h"));
+    clearTimeout(self.data("h"));
 
-        self.addClass("h").data(
-            "h",
-            setTimeout(() => {
-                if (!audioState.audio.paused) {
-                    self.removeClass("h");
-                }
-            }, 1500)
-        );
-    }
+    self.addClass("h").data(
+        "h",
+        setTimeout(() => {
+            if (!audioState.audio.paused) {
+                self.removeClass("h");
+            }
+        }, 1500)
+    );
 });
