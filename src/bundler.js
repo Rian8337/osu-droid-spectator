3f591d296c3463bee175aa4572c08f2dfefe039b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2023-11-11 12:00:44 +0800

    Generalize bundle path

--- src/bundler.js
+++ src/bundler.js
@@ -1,34 +1,36 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { readdirSync, existsSync, writeFileSync, mkdirSync } = require("fs");
+const { join } = require("path");
 const browserify = require("browserify");
 const tsify = require("tsify");
 const babelify = require("babelify");
 
 const b = browserify();
 
-b.add(`${__dirname}/ts/index.ts`);
+b.add(join(__dirname, "ts", "index.ts"));
 
-const elementsPath = `${__dirname}/ts/elements`;
+const elementsPath = join(__dirname, "ts", "elements");
 const elements = readdirSync(elementsPath);
 
 for (const element of elements) {
-    b.add(`${elementsPath}/${element}`);
+    b.add(join(elementsPath, element));
 }
 
-b
-    .plugin(tsify)
-    .transform(babelify.configure({
-        extensions: [".js", ".ts", ".cjs"],
-        presets: ["@babel/preset-env"],
-    }))
+b.plugin(tsify)
+    .transform(
+        babelify.configure({
+            extensions: [".js", ".ts", ".cjs"],
+            presets: ["@babel/preset-env"],
+        }),
+    )
     .bundle((err, data) => {
         if (err) throw err;
 
-        const dir = `${__dirname}/js`;
+        const dir = join(__dirname, "js");
 
         if (!existsSync(dir)) {
             mkdirSync(dir);
         }
 
-        writeFileSync(`${dir}/index.js`, data);
+        writeFileSync(join(dir, "index.js"), data);
     });
