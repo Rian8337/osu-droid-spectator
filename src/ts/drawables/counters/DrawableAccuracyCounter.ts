d30db05217465756b0af9e0c0047ee863d5c841b
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2024-12-31 12:25:01 +0800

    Persist `DrawableAccuracyCounter` location
    Reduces motion sickness and increases readability.

--- src/ts/drawables/counters/DrawableAccuracyCounter.ts
+++ src/ts/drawables/counters/DrawableAccuracyCounter.ts
@@ -15,23 +15,76 @@ export class DrawableAccuracyCounter extends DrawableStatisticsCounter<Spectator
     protected override readonly allowFractional = true;
     protected override readonly rollingDuration = 375;
 
+    private readonly charLengthMap = new Map<string, number>();
+    private longestCharWidth = 0;
+
     override draw(ctx: CanvasRenderingContext2D, time: number): void {
         this.update(time);
 
         const { fontSize, paddingX, paddingY } = DrawableAccuracyCounter;
 
-        this.setupContext(ctx, new Vector2(ctx.canvas.width, 0), fontSize);
+        // For accuracy counter, we need to setup in a way that the position stays as is, depending on the
+        // longest width of the number character. This is done to prevent motion sickness of the user.
+        this.setupContext(ctx, new Vector2(0), fontSize);
 
-        ctx.textAlign = "right";
-        ctx.fillText(
-            `${(this.currentValue * 100).toFixed(2)}%`,
-            -paddingX,
-            paddingY,
+        if (this.charLengthMap.size === 0) {
+            this.updateLongestCharWidth(ctx);
+        }
+
+        // Reapply context with respect to new origin.
+        ctx.restore();
+
+        const value = `${(this.currentValue * 100).toFixed(2)}%`;
+
+        this.setupContext(
+            ctx,
+            new Vector2(
+                ctx.canvas.width -
+                    // Dots and percentage signs do not obey longest character width.
+                    value.replace(/.%/, "").length * this.longestCharWidth -
+                    (this.charLengthMap.get(".") ?? this.longestCharWidth) -
+                    (this.charLengthMap.get("%") ?? this.longestCharWidth) -
+                    paddingX,
+                paddingY,
+            ),
+            fontSize,
         );
+
+        for (const char of value) {
+            const width = this.charLengthMap.get(char) ?? this.longestCharWidth;
+
+            if (char === "." || char === "%") {
+                ctx.fillText(char, 0, 0);
+                ctx.translate(width, 0);
+            } else {
+                // Center the character.
+                ctx.fillText(char, (this.longestCharWidth - width) / 2, 0);
+                ctx.translate(this.longestCharWidth, 0);
+            }
+        }
+
         ctx.restore();
     }
 
     protected override getEventValue(event: SpectatorAccuracyEvent): number {
         return event.accuracy;
+    }
+
+    private updateLongestCharWidth(ctx: CanvasRenderingContext2D): void {
+        const loadChar = (char: string) => {
+            const { width } = ctx.measureText(char);
+
+            this.charLengthMap.set(char, width);
+            this.longestCharWidth = Math.max(width, this.longestCharWidth);
+        };
+
+        this.longestCharWidth = 0;
+
+        for (let i = 0; i < 10; ++i) {
+            loadChar(i.toString());
+        }
+
+        loadChar(".");
+        loadChar("%");
     }
 }
