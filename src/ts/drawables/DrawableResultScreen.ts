9aa1d1db7d917076b22c5d40fe7e5c6d68eee809
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-01-15 19:56:26 +0800

    Reposition + resize some texts and add rank

--- src/ts/drawables/DrawableResultScreen.ts
+++ src/ts/drawables/DrawableResultScreen.ts
@@ -1,4 +1,9 @@
-import { Accuracy } from "@rian8337/osu-base";
+import {
+    Accuracy,
+    ModFlashlight,
+    ModHidden,
+    ScoreRank,
+} from "@rian8337/osu-base";
 import { dataProcessor, hitResultColors } from "../settings/SpectatorSettings";
 import { HitResult } from "../spectator/structures/HitResult";
 
@@ -32,6 +37,7 @@ export class DrawableResultScreen {
         this.drawHitResults(ctx);
         this.drawMaxCombo(ctx);
         this.drawAccuracy(ctx);
+        this.drawRank(ctx);
     }
 
     private drawBackgroundDim(ctx: CanvasRenderingContext2D) {
@@ -51,12 +57,12 @@ export class DrawableResultScreen {
         ctx.save();
 
         ctx.fillStyle = "#FFFFFF";
-        ctx.font = `bold ${(ctx.canvas.height / 10).toString()}px Torus, sans-serif`;
+        ctx.font = `bold ${(ctx.canvas.height / 7.5).toString()}px Torus, sans-serif`;
 
         ctx.fillText(
-            manager.result.score.toString(),
-            ctx.canvas.width / 4,
-            ctx.canvas.height / 5,
+            manager.result.score.toLocaleString("en-US"),
+            ctx.canvas.width / 10,
+            ctx.canvas.height / 6,
         );
 
         ctx.restore();
@@ -67,7 +73,9 @@ export class DrawableResultScreen {
 
         ctx.save();
 
-        let yPosition = ctx.canvas.height / 4;
+        ctx.textBaseline = "middle";
+
+        let yPosition = ctx.canvas.height / 3.75;
 
         const drawResult = (
             result: string,
@@ -95,21 +103,24 @@ export class DrawableResultScreen {
                     color = hitResultColors[HitResult.miss].toString();
             }
 
-            let xPosition = ctx.canvas.width / 5;
+            let xPosition = ctx.canvas.width / 10;
 
             if (position === "right") {
-                xPosition *= 2.5;
+                xPosition += ctx.canvas.width / 4;
             }
 
             ctx.fillStyle = `rgb(${color})`;
-            ctx.font = `bold ${(ctx.canvas.height / 15).toString()}px Torus, sans-serif`;
+            ctx.font = `bold ${(ctx.canvas.height / 10).toString()}px Torus, sans-serif`;
             ctx.fillText(result, xPosition, yPosition);
 
             ctx.fillStyle = "#FFFFFF";
-            ctx.font = `${(ctx.canvas.height / 20).toString()}px Torus, sans-serif`;
+            ctx.font = `${(ctx.canvas.height / 15).toString()}px Torus, sans-serif`;
             ctx.fillText(
                 (count ?? 0).toString(),
-                xPosition + ctx.canvas.width / 10,
+                xPosition +
+                    (position === "left"
+                        ? ctx.canvas.width / 10
+                        : ctx.canvas.width / 15),
                 yPosition,
             );
 
@@ -123,7 +134,7 @@ export class DrawableResultScreen {
         drawResult("100", manager?.result?.good, "left");
         drawResult("喝", manager?.result?.katu, "right");
         drawResult("50", manager?.result?.bad, "left");
-        drawResult("❌", manager?.result?.miss, "right");
+        drawResult("X", manager?.result?.miss, "right");
 
         ctx.restore();
     }
@@ -134,20 +145,20 @@ export class DrawableResultScreen {
         ctx.save();
 
         ctx.fillStyle = "#FFFFFF";
-        ctx.font = `bold ${(ctx.canvas.height / 15).toString()}px Torus, sans-serif`;
+        ctx.font = `bold ${(ctx.canvas.height / 10).toString()}px Torus, sans-serif`;
 
         ctx.fillText(
             "Max Combo",
-            ctx.canvas.width / 4,
-            (ctx.canvas.height * 3) / 4,
+            ctx.canvas.width / 10,
+            (ctx.canvas.height * 4) / 5,
         );
 
-        ctx.font = `${(ctx.canvas.height / 20).toString()}px Torus, sans-serif`;
+        ctx.font = `${(ctx.canvas.height / 15).toString()}px Torus, sans-serif`;
 
         ctx.fillText(
-            (manager?.result?.maxCombo ?? 0).toString(),
-            ctx.canvas.width / 4,
-            (ctx.canvas.height * 3) / 4 + ctx.canvas.height / 10,
+            (manager?.result?.maxCombo ?? 0).toString() + "x",
+            ctx.canvas.width / 10,
+            (ctx.canvas.height * 4) / 5 + ctx.canvas.height / 10,
         );
 
         ctx.restore();
@@ -166,20 +177,95 @@ export class DrawableResultScreen {
         ctx.save();
 
         ctx.fillStyle = "#FFFFFF";
-        ctx.font = `bold ${(ctx.canvas.height / 15).toString()}px Torus, sans-serif`;
+        ctx.font = `bold ${(ctx.canvas.height / 10).toString()}px Torus, sans-serif`;
 
         ctx.fillText(
             "Accuracy",
-            (ctx.canvas.width * 3) / 4,
-            (ctx.canvas.height * 3) / 4,
+            (ctx.canvas.width * 4) / 10,
+            (ctx.canvas.height * 4) / 5,
         );
 
-        ctx.font = `${(ctx.canvas.height / 20).toString()}px Torus, sans-serif`;
+        ctx.font = `${(ctx.canvas.height / 15).toString()}px Torus, sans-serif`;
 
         ctx.fillText(
             ((accuracy.value() || 0) * 100).toFixed(2) + "%",
-            (ctx.canvas.width * 3) / 4,
-            (ctx.canvas.height * 3) / 4 + ctx.canvas.height / 10,
+            (ctx.canvas.width * 4) / 10,
+            (ctx.canvas.height * 4) / 5 + ctx.canvas.height / 10,
+        );
+
+        ctx.restore();
+    }
+
+    private drawRank(ctx: CanvasRenderingContext2D) {
+        const manager = dataProcessor.managers.get(this.uid);
+
+        const accuracy = new Accuracy({
+            n300: manager?.result?.perfect ?? 0,
+            n100: manager?.result?.good ?? 0,
+            n50: manager?.result?.bad ?? 0,
+            nmiss: manager?.result?.miss ?? 0,
+        });
+
+        let rank: ScoreRank | "F" = "F";
+
+        ctx.save();
+        ctx.textAlign = "right";
+        ctx.font = `bold ${(ctx.canvas.height / 4).toString()}px Torus, sans-serif`;
+        ctx.fillStyle = "#ad1010";
+
+        if (manager?.result && accuracy.value()) {
+            const totalHits =
+                accuracy.n300 + accuracy.n100 + accuracy.n50 + accuracy.nmiss;
+
+            const isHidden = manager.mods.some(
+                (m) => m instanceof ModHidden || m instanceof ModFlashlight,
+            );
+
+            const hit300Ratio = accuracy.n300 / totalHits;
+
+            switch (true) {
+                case accuracy.value() === 1:
+                    rank = "X";
+                    ctx.fillStyle = isHidden ? "#a8a8a8" : "#eba731";
+                    break;
+
+                case hit300Ratio > 0.9 &&
+                    accuracy.n50 / totalHits < 0.01 &&
+                    !accuracy.nmiss:
+                    rank = "S";
+                    ctx.fillStyle = isHidden ? "#a8a8a8" : "#eba731";
+                    break;
+
+                case (hit300Ratio > 0.8 && !accuracy.nmiss) ||
+                    hit300Ratio > 0.9:
+                    rank = "A";
+                    ctx.fillStyle = "#37d422";
+                    break;
+
+                case (hit300Ratio > 0.7 && !accuracy.nmiss) ||
+                    hit300Ratio > 0.8:
+                    rank = "B";
+                    ctx.fillStyle = "#2255e3";
+                    break;
+
+                case hit300Ratio > 0.6:
+                    rank = "C";
+                    ctx.fillStyle = "#e022e3";
+                    break;
+
+                default:
+                    rank = "D";
+                    ctx.fillStyle = "#f02e2e";
+                    break;
+            }
+        }
+
+        ctx.font = `bold ${(ctx.canvas.height / 1.25).toString()}px Torus, sans-serif`;
+
+        ctx.fillText(
+            rank,
+            (ctx.canvas.width * 9) / 10,
+            (ctx.canvas.height * 6) / 10,
         );
 
         ctx.restore();
